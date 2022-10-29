4722c55738b14bfc0abf8f4abc96f288897a3c5c
digest15 <digest15@yandex.ru>
2022-10-29 12:09:59 +0800

    #285211 #355643
    Refactor model Post

--- src/main/java/ru/job4j/models/Post.java
+++ src/main/java/ru/job4j/models/Post.java
@@ -7,20 +7,30 @@ import java.util.Objects;
 public class Post {
     private int id;
     private String title;
+    private String description;
     private String link;
     private LocalDateTime created;
 
-    public Post(int id, String title, String link, LocalDateTime created) {
+    public Post(int id, String title, String description, String link, LocalDateTime created) {
         this.id = id;
         this.title = title;
+        this.description = description;
+        this.link = link;
+        this.created = created;
+    }
+
+    public Post(String title, String description, String link, LocalDateTime created) {
+        this.title = title;
+        this.description = description;
         this.link = link;
         this.created = created;
     }
 
     @Override
     public String toString() {
-        return String.format("%s; %s; %s%n",
+        return String.format("%s; %s; %s; %s%n",
                 title,
+                description,
                 created.format(DateTimeFormatter.ISO_DATE_TIME),
                 link);
     }
@@ -34,11 +44,51 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return Objects.equals(link, post.link);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(link);
+        return Objects.hash(id, link);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 }
