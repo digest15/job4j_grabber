64d6cd0cb87c2b97fff82175ab3f6ca5340463c1
digest15 <digest15@yandex.ru>
2022-10-29 14:57:51 +0800

    #285212
    Get description

--- src/main/java/ru/job4j/HabrCareerParse.java
+++ src/main/java/ru/job4j/HabrCareerParse.java
@@ -4,6 +4,8 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import ru.job4j.datetimeparsers.DateTimeParser;
 import ru.job4j.datetimeparsers.HabrCareerDateTimeParser;
@@ -11,6 +13,8 @@ import ru.job4j.datetimeparsers.HabrCareerDateTimeParser;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.stream.IntStream;
 
 public class HabrCareerParse {
@@ -42,6 +46,8 @@ public class HabrCareerParse {
                         .child(0);
                 LocalDateTime datetime = dateTimeParser.parse(dateElement.attr("datetime"));
 
+                String description = retrieveDescription(link);
+
                 System.out.printf("%s; %s; %s%n",
                         vacancyName,
                         datetime.format(DateTimeFormatter.ISO_DATE_TIME),
@@ -49,6 +55,42 @@ public class HabrCareerParse {
             });
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String retrieveDescription(String link) {
+        StringJoiner description = new StringJoiner(System.lineSeparator());
+        try {
+            Connection connection = Jsoup.connect(link);
+            Document document = connection.get();
+            Element element = document.select(".collapsible-description__content").first();
+            retrievePlainText(description, element);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return description.toString();
+    }
+
+    private static void retrievePlainText(StringJoiner text, Node node) {
+        List<Node> elements = node.childNodes();
+        if (elements.size() > 0) {
+            elements.forEach(e -> retrievePlainText(text, e));
+        } else {
+            boolean needSpace = false;
+            if (node instanceof TextNode) {
+                String value = ((TextNode) node).text();
+                if (value.equals(" ")) {
+                    if (needSpace) {
+                        text.add(" ");
+                    }
+                    text.add(value);
+                    needSpace = true;
+                } else if (((Element) ((TextNode) node).parentNode()).tag().getName().equals("li")) {
+                    text.add("- ");
+                    text.add(value);
+                    needSpace = false;
+                }
+            }
         }
     }
 }
