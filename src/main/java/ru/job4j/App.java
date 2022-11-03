8a11b5fa7e25820440aec65a346189e1264d5b92
digest15 <digest15@yandex.ru>
2022-11-03 12:16:12 +0800

    #289477
    Implement Grab

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,34 +1,17 @@
 package ru.job4j;
 
+import org.quartz.Scheduler;
 import ru.job4j.datetimeparsers.HabrCareerDateTimeParser;
+import ru.job4j.grabbers.GrabberSqlDb;
 import ru.job4j.parsers.HabrCareerParse;
-import ru.job4j.parsers.Parse;
-import ru.job4j.repository.jdbc.PsqlStore;
-
-import java.io.InputStream;
-import java.util.Properties;
+import ru.job4j.repository.Store;
 
 public class App {
-    private static final String MAIN_PROPERTIES_PATH = "rabbit.properties";
-   public static void main(String[] args) {
-       Parse parser = new HabrCareerParse(new HabrCareerDateTimeParser());
-       try (PsqlStore store = new PsqlStore(readConfig(MAIN_PROPERTIES_PATH))) {
-           parser.list()
-                   .forEach(store::save);
-           store.getAll()
-                   .forEach(System.out::println);
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-    }
-
-    private static Properties readConfig(String configPath) {
-        Properties cfg = new Properties();
-        try (InputStream in = App.class.getClassLoader().getResourceAsStream(configPath)) {
-            cfg.load(in);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return cfg;
+   public static void main(String[] args) throws Exception {
+       GrabberSqlDb grab = new GrabberSqlDb();
+       grab.cfg();
+       Scheduler scheduler = grab.scheduler();
+       Store store = grab.store();
+       grab.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
     }
 }
---
+++ src/main/java/ru/job4j/grabbers/GrabberSqlDb.java
@@ -0,0 +1,65 @@
+package ru.job4j.grabbers;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.parsers.Parse;
+import ru.job4j.repository.Store;
+import ru.job4j.repository.jdbc.PsqlStore;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class GrabberSqlDb implements Grab {
+    private final Properties cfg = new Properties();
+
+    public Store store() {
+        return new PsqlStore(cfg);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() throws IOException {
+        try (InputStream in = GrabberSqlDb.class.getClassLoader().getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("rabbit.interval")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            parse.list()
+                    .forEach(store::save);
+        }
+    }
+}
